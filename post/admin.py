4ad4bd8aa4f7e2d40972387a7eb9afa46141fb77
nurbaybara <nsbaybar@gmail.com>
2020-04-07 16:32:24 +0300

    Product Admin Resim Galerisi eklendi.

--- post/admin.py
+++ post/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
-from post.models import Category, Post
+from post.models import Category, Post, Images
+
+
+class PostImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'price','amount','status']
+    list_display = ['title', 'category', 'price', 'amount', 'status']
     list_filter = ['status', 'category']
+    inlines = [PostImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'post', 'image']
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Images, ImagesAdmin)
--- post/models.py
+++ post/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -19,22 +19,31 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-class Post(models.Model):
-        STATUS = (
-            ('True', 'Evet'),
-            ('False', 'Hayır'),
-        )
-        category = models.ForeignKey(Category, on_delete=models.CASCADE)
-        title = models.CharField(max_length=30)
-        keyword = models.CharField(max_length=255)
-        description = models.CharField(max_length=255)
-        image = models.ImageField(blank=True, upload_to="images/")
-        price = models.FloatField()
-        amount = models.IntegerField()
-        detail = models.TextField()
-        status = models.CharField(max_length=10, choices=STATUS)
-        create_at = models.DateTimeField(auto_now_add=True)
-        update_at = models.DateTimeField(auto_now=True)
 
-        def __str__(self):
-            return self.title
+class Post(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    keyword = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to="images/")
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Images(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # post silinirse Imageste silinmeli
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to="images/")
+    def __str__(self):
+        return self.title
