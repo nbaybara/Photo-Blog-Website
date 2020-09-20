c7722e4feed52b5bff1f95eed0dedabf97642847
Nur Sefa BAYBARA <nsbaybar@gmail.com>
2020-09-20 23:50:13 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,6 +23,18 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'tgpsm%7p0f5a2q_mi!7d7@16k$6n_to5dvuh@my)y@82u5!no@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 DEBUG = True
 ALLOWED_HOSTS = []
